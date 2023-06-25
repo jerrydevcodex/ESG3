652ffeaafcb7c2ee3e2bdea3e4ac0e62524f8df3
Gichan-Song <thdrlcks97@gmail.com>
2023-06-25 18:38:28 +0900

    react-router-dom

---
+++ company-introduction/src/App.js
@@ -0,0 +1,52 @@
+
+import './App.css';
+import './style.css'
+import Nav from './components/Nav'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/service" element={<Home/>}/>
+          <Route path="/contact" element={<Home/>}/>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
+
+
+const Home = () => {
+  return (
+    <body>
+    <div class="wrap">
+        <div class="intro_bg">
+            <Nav/>
+            
+        </div>
+    </div>
+    <footer>
+        <p> 여기에 푸터를 넣을거에요</p>
+
+    </footer>
+    </body>
+  )
+}
+
+
+const About = () => {
+  return (
+    <div>
+      <div class="intro_bg">
+            <Nav/>
+            <h1>ABOUT</h1>
+        </div>
+    </div>
+  )
+}
---
+++ company-introduction/src/components/Nav.js
@@ -0,0 +1,18 @@
+import logo from '../logo.png';
+import { Link } from 'react-router-dom';
+
+const Nav = () => {
+    return (
+        <div class="header">
+                <ul class="nav">
+                    <img class = "logo" src={logo} alt="logo"/>
+                    <li><Link to='/'>HOME</Link></li>
+                    <li><Link to='/about'>ABOUT</Link></li>
+                    <li><Link>SERVICE</Link></li>
+                    <li><Link>CONTACT</Link></li>
+                </ul>
+            </div>
+    )
+}
+
+export default Nav;
